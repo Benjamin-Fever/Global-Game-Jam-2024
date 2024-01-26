a3c93a2a213bfcf25554b36c20036ea5048281aa
elvycall <elvycall@ecs.vuw.ac.nz>
2024-01-27 12:27:37 +1300

    -

--- options.gd
+++ options.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var res = [Vector2(690,420)]
+var res = [Vector2(690,420),Vector2(1614,1175),Vector2(1367,1503),Vector2(1152,648)]
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://start_menu.tscn")
